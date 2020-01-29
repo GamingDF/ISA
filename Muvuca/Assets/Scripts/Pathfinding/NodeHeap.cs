180c9fc32936fd2af267cc983407061e173cacd9
gerudoking <edsantos2003@gmail.com>
2020-01-29 10:36:53 -0300

    Pathfinding de exemplo

---
+++ Muvuca/Assets/Scripts/Pathfinding/NodeHeap.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeHeap {
+	Node[] items;
+	int currentItemCount;
+
+	public NodeHeap(int maxHeapSize) {
+		items = new Node[maxHeapSize];
+	}
+
+	public void Add(Node item) {
+		item.heapIndex = currentItemCount;
+		items[currentItemCount] = item;
+		SortUp(item);
+		currentItemCount++;
+	}
+
+	public Node RemoveFirst() {
+		Node firstItem = items[0];
+		currentItemCount--;
+		items[0] = items[currentItemCount];
+		items[0].heapIndex = 0;
+		SortDown(items[0]);
+		return firstItem;
+	}
+
+	public bool Contains(Node item) {
+		return Equals(items[item.heapIndex], item);
+	}
+
+	public void UpdateItem(Node item) {
+		SortUp(item);
+	}
+
+	public int Count {
+		get {
+			return currentItemCount;
+		}
+	}
+
+	void Swap(Node itemA, Node itemB) {
+		items[itemA.heapIndex] = itemB;
+		items[itemB.heapIndex] = itemA;
+		int itemAIndex = itemA.heapIndex;
+		itemA.heapIndex = itemB.heapIndex;
+		itemB.heapIndex = itemAIndex;
+	}
+
+	void SortUp(Node item) {
+		int parentIndex = (item.heapIndex-1)/2;
+
+		while(true) {
+			Node parentItem = items[parentIndex];
+			if(item.CompareTo(parentItem) > 0) {
+				Swap(item, parentItem);
+			}
+			else {
+				return;
+			}
+
+			parentIndex = (item.heapIndex-1)/2;
+		}
+	}
+
+	void SortDown(Node item) {
+		while(true) {
+			int childIndexLeft = item.heapIndex*2 + 1;
+			int childIndexRight = item.heapIndex*2 + 2;
+			int swapIndex = 0;
+
+			if(childIndexLeft < currentItemCount) {
+				swapIndex = childIndexLeft;
+
+				if(childIndexRight < currentItemCount) {
+					if(items[childIndexLeft].CompareTo(items[childIndexRight]) < 0) {
+						swapIndex = childIndexRight;
+					}
+				}
+
+				if(item.CompareTo(items[swapIndex]) < 0) {
+					Swap(item, items[swapIndex]);
+				}
+				else {
+					return;
+				}
+			}
+			else {
+				return;
+			}
+		}
+	}
+}
---
+++ Muvuca/Assets/Scripts/Pathfinding/PathRequestManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathRequestManager : MonoBehaviour {
+	private Queue<PathRequest> pathRequestQueue;/*= new Queue<PathRequest>()*/
+	private PathRequest currentPathRequest;
+
+	public static PathRequestManager instance;
+	Pathfinding pathfinding;
+
+	bool isProcessing;
+
+	void Awake() {
+		instance = this;
+		pathRequestQueue = new Queue<PathRequest>();
+		pathfinding = GetComponent<Pathfinding>();
+	}
+
+	public void RequestPath(Vector3 pathStart, Vector3 pathEnd, NodeGrid grid, Action<Vector3[], bool> callback) {
+		if(grid != null) {
+			grid.LoadGrid();
+			PathRequest newRequest = new PathRequest(pathStart, pathEnd, grid, callback);
+			pathRequestQueue.Enqueue(newRequest);
+			TryProcessNext();
+		}
+	}
+
+	void TryProcessNext() {
+		if(!isProcessing && pathRequestQueue.Count > 0) {
+			currentPathRequest = pathRequestQueue.Dequeue();
+			isProcessing = true;
+			instance.pathfinding.StartFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd, currentPathRequest.grid);
+		}
+	}
+
+	public void FinishProcessingPath(Vector3[] path, bool success) {
+		currentPathRequest.callback(path, success);
+		isProcessing = false;
+		TryProcessNext();
+	}
+
+	struct PathRequest {
+		public Vector3 pathStart;
+		public Vector3 pathEnd;
+		public NodeGrid grid;
+		public Action<Vector3[], bool> callback;
+
+		public PathRequest(Vector3 _pathStart, Vector3 _pathEnd, NodeGrid _grid, Action<Vector3[], bool> _callback) {
+			pathStart = _pathStart;
+			pathEnd = _pathEnd;
+			grid = _grid;
+			callback = _callback;
+		}
+	}
+}
