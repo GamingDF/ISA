0789b1e21a623aee24ab3e16a7edcc86c6b60c34
gerudoking <edsantos2003@gmail.com>
2020-01-29 10:31:35 -0300

    Início do sistema para o puzzle

---
+++ Muvuca/Assets/Scripts/Puzzle/PuzzleArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleArea : MonoBehaviour
+{
+    [Tooltip("Altura e largura da grid")]
+    [SerializeField]
+    private int gridSize;
+    private List<List<PuzzleCell>> cells;
+
+    private void Awake() {
+        cells = new List<List<PuzzleCell>>();
+        for(int i = 0; i < gridSize; i++) {
+            cells[i] = new List<PuzzleCell>();
+            for(int j = 0; j < gridSize; j++) {
+                cells[i].Add(new PuzzleCell());
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Muvuca/Assets/Scripts/Puzzle/PuzzleCell.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleCell
+{
+    public enum PlantType {
+        P,
+        S,
+        C
+    }
+
+    public bool influenced;
+    public List<PlantType> plants;
+
+    public PuzzleCell() {
+        plants = new List<PlantType>();
+    }
+}
