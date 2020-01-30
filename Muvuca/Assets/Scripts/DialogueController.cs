83e75c768fa2ed66636527d244e49324805bc932
gerudoking <edsantos2003@gmail.com>
2020-01-30 12:23:47 -0300

    Sistema de diálogo com o básico feito

---
+++ Muvuca/Assets/Scripts/DialogueController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour
+{
+    public static DialogueController Instance
+    {
+        get;
+        private set;
+    }
+
+    [SerializeField]
+    private GameObject dialogueBox;
+    [SerializeField]
+    private bool playSoundOnText;
+
+    public bool onDialogue;
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        }
+        else {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        //Teste de diálogo
+        if (Input.GetKeyDown(KeyCode.D)) {
+            CallDialogue("[fteste]aAAAaaaaAAAAAaaaaAaAAAaA");
+        }
+    }
+
+    public void ChangePortrait(string name) {
+        dialogueBox.transform.Find("Portrait").GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Portraits/" + name);
+    }
+
+    public void InsertChar(char c) {
+        dialogueBox.transform.Find("Text").GetComponent<Text>().text += c;
+        if (playSoundOnText) {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    public void CallDialogue(string d) {
+        onDialogue = true;
+        dialogueBox.SetActive(true);
+        dialogueBox.transform.Find("Text").GetComponent<Text>().text = "";
+
+        StartCoroutine(GetComponent<TextParser>().Parse(d));
+    }
+}
---
+++ Muvuca/Assets/Scripts/TextParser.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextParser : MonoBehaviour
+{
+    private char commandCharStart = '[';
+    private char commandCharEnd = ']';
+    private char portraitCommand = 'f';
+
+    private string portraitName;
+    private float textSpeed = 0.25f;
+
+    public IEnumerator Parse(string original) {
+        bool checkForCommand = false;
+        bool checkForPortrait = false;
+
+        foreach(char c in original) {
+            //Área de recebimento de comandos
+            if (c == commandCharEnd) {  //Termina o comando
+                if (checkForPortrait) {
+                    Debug.Log(portraitName);
+                    GetComponent<DialogueController>().ChangePortrait(portraitName);
+                    portraitName = null;
+                    checkForPortrait = false;
+                }
+
+                checkForCommand = false;
+                continue;
+            }
+
+            if (checkForCommand) {
+                if(c == portraitCommand && portraitName == null) {  //Comando de retrato
+                    checkForPortrait = true;
+                    continue;
+                }
+
+                if (checkForPortrait) {
+                    portraitName += c;
+                    continue;
+                }
+            }
+
+            if(c == commandCharStart) {  //Começa o comando
+                checkForCommand = true;
+                continue;
+            }
+            //Fim dos comandos
+
+            //Processamento de texto
+            GetComponent<DialogueController>().InsertChar(c);
+
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+}
