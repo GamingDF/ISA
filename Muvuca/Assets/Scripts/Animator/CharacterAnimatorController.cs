2ace6544be2defa0d14cad5006cb91f22dcc256f
bruno takashi <bt.tengan@gmail.com>
2020-02-03 11:40:13 -0300

    Animator apenas com idle e controlador do animator

---
+++ Muvuca/Assets/Scripts/Animator/CharacterAnimatorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimatorController : MonoBehaviour {
+	Animator _animator = null;
+	Rigidbody2D _rb = null;
+
+	void Start() {
+		_animator = GetComponent<Animator>();
+		_rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		_animator.SetFloat("angle", Mathf.Atan2(_rb.velocity.y, _rb.velocity.x));
+		_animator.SetFloat("velocity", _rb.velocity.magnitude);
+	}
+}
