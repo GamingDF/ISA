9889bf52fc1adc6e44d152d80c3559d417889891
bruno takashi <bt.tengan@gmail.com>
2020-02-05 10:34:56 -0300

    Implementado input de único tap.

--- Muvuca/Assets/Scripts/ShowTouch.cs
+++ Muvuca/Assets/Scripts/ShowTouch.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShowTouch : MonoBehaviour {
+	public int[] id;
+
 	void Update() {
 		for (int i = 0; i < Input.touchCount; i++) {
 			Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+			id[i] = Input.touches[i].fingerId;
 			switch (i) {
 				case 0:
 					Debug.DrawLine(Vector3.zero, touchPosition, Color.red);
