6f31e85d390ac3248a42557dcd1af67b185574bc
bruno takashi <bt.tengan@gmail.com>
2020-02-06 12:16:32 -0300

    [WIP] movimentação usando tap e pathfinding.
    - o nodeGrid apresenta alguns problemas para definir a região de colisão o que gera caminhos no meio do colisor.

---
+++ Muvuca/Assets/Scripts/MoveTap.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MoveTap : MonoBehaviour {
+	[SerializeField] NodeGrid _grid = null;
+	[SerializeField] Rigidbody2D _rb = null;
+	[SerializeField] float _speed = 0;
+	Action<Vector3[], bool> _pathfindingCallback;
+	Vector3[] _path = null;
+	bool _newPath = false;
+	bool _doMovement = false;
+
+	void Awake() {
+		_pathfindingCallback += ReceivedPath;
+	}
+
+	void Update() {
+		if (TapInput.Instance.IsSingle) {
+			PathRequestManager.instance.RequestPath(transform.position, TapInput.Instance.WorldPosition, _grid, _pathfindingCallback);
+		}
+	}
+
+	void FixedUpdate() {
+		if (_newPath) {
+			if (_path.Length > 0) {
+				_doMovement = true;
+			}
+			else {
+				_doMovement = false;
+				_rb.velocity = Vector2.zero;
+			}
+		}
+
+		if (_doMovement) {
+			// move between points.
+		}
+	}
+
+	void ReceivedPath(Vector3[] path, bool success) {
+		_path = path;
+		_newPath = true;
+	}
+
+	void MoveOnPath() {
+
+	}
+}
--- Muvuca/Assets/Scripts/Pathfinding/NodeGrid.cs
+++ Muvuca/Assets/Scripts/Pathfinding/NodeGrid.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class NodeGrid : MonoBehaviour{
+public class NodeGrid : MonoBehaviour {
 	public LayerMask unwalkableMask;
 	public bool displayGridGizmos;
 
@@ -22,15 +22,15 @@ public class NodeGrid : MonoBehaviour{
 	public Vector3[] pathS;
 
 	public void LoadGrid() {
-		nodeDiameter = nodeRadius*2;
-		gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
-		gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
+		nodeDiameter = nodeRadius * 2;
+		gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+		gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
 		grid = new Node[gridSizeX, gridSizeY];
-		Vector3 worldBottomLeft = transform.position - new Vector3(gridWorldSize.x/2, gridWorldSize.y / 2, 0);
+		Vector3 worldBottomLeft = transform.position - new Vector3(gridWorldSize.x / 2, gridWorldSize.y / 2, 0);
 
-		for(int i = 0; i < gridSizeX; i++) {
-			for(int j = 0; j < gridSizeY; j++) {
-				Vector3 worldPos = worldBottomLeft + new Vector3(i*nodeDiameter + nodeRadius, j * nodeDiameter + nodeRadius, 0);
+		for (int i = 0; i < gridSizeX; i++) {
+			for (int j = 0; j < gridSizeY; j++) {
+				Vector3 worldPos = worldBottomLeft + new Vector3(i * nodeDiameter + nodeRadius, j * nodeDiameter + nodeRadius, 0);
 				bool walkable = !(Physics2D.OverlapCircle(worldPos, nodeRadius, unwalkableMask));
 				grid[i, j] = new Node(i, j, worldPos, walkable);
 			}
@@ -39,15 +39,15 @@ public class NodeGrid : MonoBehaviour{
 
 	public List<Node> GetNeighbours(Node node) {
 		List<Node> neighbours = new List<Node>();
-		for(int x = -1; x <= 1; x++) {
-			for(int y = -1; y <= 1; y++) {
-				if(x == 0 && y == 0)
+		for (int x = -1; x <= 1; x++) {
+			for (int y = -1; y <= 1; y++) {
+				if (x == 0 && y == 0)
 					continue;
 
 				int checkX = node.gridX + x;
 				int checkY = node.gridY + y;
 
-				if(checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY) {
+				if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY) {
 					neighbours.Add(grid[checkX, checkY]);
 				}
 			}
@@ -57,33 +57,33 @@ public class NodeGrid : MonoBehaviour{
 	}
 
 	public Node GetNodeFromWorldPoint(Vector3 worldPos) {
-		if(gameObject == null){
+		if (gameObject == null) {
 			return null;
 		}
 
-		float percentX = Mathf.Clamp01((worldPos.x - transform.position.x + gridWorldSize.x/2) / gridWorldSize.x);
-		float percentY = Mathf.Clamp01((worldPos.y - transform.position.y + gridWorldSize.y/2) / gridWorldSize.y);
+		float percentX = Mathf.Clamp01((worldPos.x - transform.position.x + gridWorldSize.x / 2) / gridWorldSize.x);
+		float percentY = Mathf.Clamp01((worldPos.y - transform.position.y + gridWorldSize.y / 2) / gridWorldSize.y);
 
-		int x = Mathf.RoundToInt((gridSizeX-1) * percentX);
-		int y = Mathf.RoundToInt((gridSizeY-1) * percentY);
+		int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+		int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
 
 		return grid[x, y];
 	}
 
-	void OnDrawGizmos() {
+	void OnDrawGizmosSelected() {
 		Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y, 1));
 
-		if(grid != null && displayGridGizmos) {
-			foreach(Node n in grid) {
+		if (grid != null && displayGridGizmos) {
+			foreach (Node n in grid) {
 				Gizmos.color = (n.walkable) ? Color.white : Color.red;
 
-				if(pathA != null) {
-					foreach(Node m in pathA) {
-						if(n == m) {
+				if (pathA != null) {
+					foreach (Node m in pathA) {
+						if (n == m) {
 							Gizmos.color = Color.black;
-							if(pathS != null) {
-								foreach(Vector3 v in pathS) {
-									if(n == GetNodeFromWorldPoint(v)) {
+							if (pathS != null) {
+								foreach (Vector3 v in pathS) {
+									if (n == GetNodeFromWorldPoint(v)) {
 										Gizmos.color = Color.green;
 									}
 								}
@@ -91,7 +91,7 @@ public class NodeGrid : MonoBehaviour{
 						}
 					}
 				}
-				Gizmos.DrawCube(n.worldPos, new Vector3(nodeDiameter-0.01f, nodeDiameter - 0.01f, 0.1f));
+				Gizmos.DrawCube(n.worldPos, new Vector3(nodeDiameter - 0.01f, nodeDiameter - 0.01f, 0.1f));
 			}
 		}
 	}
