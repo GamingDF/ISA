b9b696efec158a16255f85d62e751c0e9ce93ddb
bruno takashi <bt.tengan@gmail.com>
2020-02-14 12:22:10 -0300

    Evento de interação e protótipo interactable.

---
+++ Muvuca/Assets/Scripts/IsInteractable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsInteractable : MonoBehaviour {
+	PlayMakerFSM _fsm;
+	Collider2D _collider;
+
+	void Start() {
+		_fsm = gameObject.GetComponent<PlayMakerFSM>();
+		if (!_fsm) {
+			Debug.LogWarning("No PlayMakerFSM in the object: " + gameObject.name);
+		}
+
+		_collider = gameObject.GetComponent<Collider2D>();
+		if (!_collider) {
+			Debug.LogWarning("No Collider2D in the object: " + gameObject.name);
+		}
+	}
+
+	void Update() {
+		if (TapInput.Instance.IsSingle) {
+			if (_collider.OverlapPoint(TapInput.Instance.WorldPosition)) {
+				_fsm.Fsm.Event("Interacted");
+			}
+		}
+	}
+}
