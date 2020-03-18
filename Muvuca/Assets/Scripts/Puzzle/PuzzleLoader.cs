364574dc55f4b550d66e7b7d94099525fef0e11d
gerudoking <edsantos2003@gmail.com>
2020-03-18 08:59:45 -0300

    Sistema de carregamento de puzzles a partir de txts implementado

---
+++ Muvuca/Assets/Scripts/Puzzle/PuzzleLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PuzzleLoader : MonoBehaviour
+{
+    public static PuzzleLoader Instance { get; private set; }
+
+    public string LoadedPuzzle { get; private set; }
+
+    public void Awake() {
+        if(Instance  != null) {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        //Teste
+        LoadPuzzleByName("teste");
+    }
+
+    public void LoadPuzzleByName(string puzzleName) {
+        //Carrega um txt compilado junto ao executável
+        TextAsset txt = (TextAsset)Resources.Load("PuzzleData/" + puzzleName, typeof(TextAsset));
+        LoadedPuzzle = txt.text;
+
+        Debug.Log(LoadedPuzzle);
+        CallPuzzleTransition();
+    }
+
+    private void CallPuzzleTransition() {
+        //Troca para a cena do Puzzle, construindo ela com base nos parâmetros
+        //presentes na string LoadedPuzzle
+
+        //Provisório, deve ser integrado ao sistema de transições de cena
+        //SceneManager.LoadScene("Puzzle");
+    }
+}
